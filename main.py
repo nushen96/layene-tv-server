ff4fd271f3cd73fe89b66f4db9d07bfd38b0b326
Papi Diagne <abdiagne96@gmail.com>
2021-04-12 20:04:47 +0000

    updated data.json and fixed cors issue

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 f = open('data.json','r')
 dummy_data = json.load(f)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
